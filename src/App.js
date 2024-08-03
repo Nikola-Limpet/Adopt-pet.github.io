7cacb97712e641b10412bdf90e45b122f1a3811f
Nikola Limpet <2023465lim@aupp.edu.kh>
2024-08-03 14:03:56 +0700

    move script to App.js and render object dog via passing value

---
+++ src/App.js
@@ -0,0 +1,36 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name), // name
+    React.createElement("h2", {}, props.animal), // animal
+    React.createElement("h2", {}, props.breed) // breed,
+  ]);
+};
+
+const App = () => {
+  return React.createElement(
+    'div',
+    {},
+    [
+    React.createElement('h1', {}, 'Adopt Me!'),
+    React.createElement(Pet, {
+      name: 'Chimmie',
+      animal: 'Dog', 
+      breed: 'Pomeranian'
+    }),
+    React.createElement(Pet, {
+      name: 'Lolen', 
+      animal: 'Lion', 
+      breed: 'African'
+    }),
+    React.createElement(Pet,{
+      name: 'Poki', 
+      animal: 'Donkey', 
+      breed: 'American'
+  }),
+  ]
+  );
+};
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
