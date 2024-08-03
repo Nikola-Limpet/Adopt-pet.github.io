ee5b406d0433cb595c55c328bff17169db35af00
Nikola Limpet <2023465lim@aupp.edu.kh>
2024-08-03 14:31:37 +0700

    set up

--- src/App.js
+++ src/App.js
@@ -7,35 +7,31 @@ const Pet = (props) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, props.name), // name
     React.createElement("h2", {}, props.animal), // animal
-    React.createElement("h2", {}, props.breed) // breed,
+    React.createElement("h2", {}, props.breed), // breed,
   ]);
 };
 
 const App = () => {
-  return React.createElement(
-    'div',
-    {},
-    [
-    React.createElement('h1', {}, 'Adopt Me!'),
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
     React.createElement(Pet, {
-      name: 'Chimmie',
-      animal: 'Dog', 
-      breed: 'Pomeranian'
+      name: "Chimmie",
+      animal: "Dog",
+      breed: "Pomeranian",
     }),
     React.createElement(Pet, {
-      name: 'Lolen', 
-      animal: 'Lion', 
-      breed: 'African'
+      name: "Lolen",
+      animal: "Lion",
+      breed: "African",
     }),
-    React.createElement(Pet,{
-      name: 'Poki', 
-      animal: 'Donkey', 
-      breed: 'American'
-  }),
-  ]
-  );
+    React.createElement(Pet, {
+      name: "Poki",
+      animal: "Donkey",
+      breed: "American",
+    }),
+  ]);
 };
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(React.createElement(App));
